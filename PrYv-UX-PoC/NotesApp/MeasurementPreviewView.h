e61c2dd7e14fd8d55865ead89e5aa6fd89ec28a4
Mladen Djordjevic <djordjevic.mladen@gmail.com>
2013-06-03 23:00:34 +0200

    measurement preview xib added

---
+++ PrYv-UX-PoC/NotesApp/MeasurementPreviewView.h
@@ -0,0 +1,13 @@
+//
+//  MeasurementPreviewView.h
+//  NotesApp
+//
+//  Created by Mladen Djordjevic on 6/3/13.
+//  Copyright (c) 2013 PrYv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeasurementPreviewView : UIView
+
+@end
