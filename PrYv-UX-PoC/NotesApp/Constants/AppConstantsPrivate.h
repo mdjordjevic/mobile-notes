d6e317367b6cf426892e04fcab678e62c7e51475
Perki <pm@perki.com>
2014-01-27 15:16:50 +0100

    added comment to AppConstantsPrivate.h file

--- PrYv-UX-PoC/NotesApp/Constants/AppConstantsPrivate.h
+++ PrYv-UX-PoC/NotesApp/Constants/AppConstantsPrivate.h
@@ -6,6 +6,18 @@
 //  Copyright (c) 2014 PrYv. All rights reserved.
 //
 
+
+// Create an appropriate  file with the following content:
+// _____________ AppConstantsPrivate.m ______
+//
+// #import "AppConstantsPrivate.h"
+// @implementation AppConstantsPrivate
+// NSString* const kPYAppConstantsPrivateTestFlightToken = @"...";
+// @end
+
+
+
+
 #import <Foundation/Foundation.h>
 
 @interface AppConstantsPrivate : NSObject
